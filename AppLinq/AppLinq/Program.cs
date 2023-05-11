e5c570a713dc51d5ab5b688d3952278866164f77
KernelByte <yonihermelendez@gmail.com>
2023-05-11 15:13:14 -0500

    Se alñade proyecto para aprender sobre el uso de la libreria LINQ

---
+++ AppLinq/AppLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿/****************  Uso de LINQ *****************************/
+
+// Creacion de la coleccion
+var frutas = new string[] { "manzana", "Pera", "Mango", "Mango Tomy", "Mango azucar" };
+
+// Uso de la libreria LINQ
+var listaFrutas = frutas.Where(p => p.StartsWith("Mango")).ToList();
+
+// Imprimir los valores de la lista
+listaFrutas.ForEach(p => Console.WriteLine(p));
+
+/********************************************************** */
