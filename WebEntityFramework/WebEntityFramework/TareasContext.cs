acce4c14f548c35ba64236b87ab025841bb27f0d
KernelByte <yonihermelendez@gmail.com>
2023-05-12 11:28:33 -0500

    Implementacion de entity framework en context para que tome los modelos como tablas de las base de datos

---
+++ WebEntityFramework/WebEntityFramework/TareasContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebEntityFramework.Models;
+
+namespace WebEntityFramework
+{
+    public class TareasContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options): base(options) { }
+    }
+}
