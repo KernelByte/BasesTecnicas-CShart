53ba04973b3991b789c8120c1a1c51a2b47c07a8
Yoniher Melendez <yonihermelendez@gmail.com>
2023-05-16 13:04:21 -0500

    Se añaden restricciones a los campos a crear en la base de datos usando los modelos creados

--- WebEntityFramework/WebEntityFramework/Models/Categoria.cs
+++ WebEntityFramework/WebEntityFramework/Models/Categoria.cs
@@ -1,8 +1,14 @@
-﻿namespace WebEntityFramework.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebEntityFramework.Models
 {
     public class Categoria
     {
+        [Key]
         public Guid CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
 
