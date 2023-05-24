d527903d581d1e5dd5bf760e9ef5e50e1825bdab
Yoniher Melendez <yonihermelendez@gmail.com>
2023-05-24 12:38:08 -0500

    Se resuelve error de cadena de conexion con la base de datos SQL Server

--- WebEntityFramework/WebEntityFramework/Program.cs
+++ WebEntityFramework/WebEntityFramework/Program.cs
@@ -6,7 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareaDB"));
+
+// Cadena para conectar base de datos en memoria
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareaDB"));
+
+// Cadena para conexion a base de datos SQL SERVER
+builder.Services.AddSqlServer<TareasContext>("Data Source=DESKTOP-N1GM6PJ;Initial Catalog=PruebaEntity;User Id=user1; Password=Hack20394; TrustServerCertificate=True");
+
 
 var app = builder.Build();
 
